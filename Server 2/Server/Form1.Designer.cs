e76deb55fc6eba16dbf78687c60d29937d2eb0bb
MohammedSkouti <121097406+MohammedSkouti@users.noreply.github.com>
2023-06-02 22:23:27 +0300

    Add files via upload

---
+++ Server 2/Server/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(378, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show Log";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(438, 238);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(9, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 260);
+            this.panel1.TabIndex = 6;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(456, 308);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Server 2/Server/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Media;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static TcpClient c;
+        public static NetworkStream ns;
+        public static StreamReader sr;
+        public static StreamWriter sw;
+
+        public static TcpListener server = new TcpListener(IPAddress.Any, 20555);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            server.Start();
+            Thread tth = new Thread(Listener);
+            tth.IsBackground = true;
+            tth.Start();
+            panel1.Visible = false;
+            label4.Text = "Port: 20555";
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    label3.Text = ip.ToString();
+                }
+            }
+            
+        }
+        private void Balloon(string user)
+        {
+
+            notifyIcon1.Icon = new Icon(@"C:\2\star.ico");
+                notifyIcon1.BalloonTipText = user+" Connected !!";
+                notifyIcon1.ShowBalloonTip(1000);
+            
+        }
+         void Listener()
+        {
+            int qx = -1;
+            while (true)
+            {
+                
+                c = server.AcceptTcpClient();
+                ns = c.GetStream();
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+                
+                
+                
+                FileStream Fs = new FileStream(@"F:\users.txt", FileMode.Open, FileAccess.Read);
+                StreamReader ss = new StreamReader(Fs);
+
+                string user = sr.ReadLine();
+                string password = sr.ReadLine();
+                while (!ss.EndOfStream)
+                {
+                    String[] a = ss.ReadLine().Split(',');
+                    string us = a[0];
+                    string pass = a[1];
+                    if (user == us && pass == password)
+                    {
+                        qx = 1;
+                        sw.WriteLine("Wellcome " + user);
+                        sw.Flush();
+                        Thread th = new Thread(first);
+                        th.Start();
+                        using (StreamWriter sw2 = new StreamWriter(@"F:\spmm.txt", true))
+                        {
+                            sw2.WriteLine(user + " Connected !!");
+                        }
+                        Balloon(user);
+                    }
+                    
+                }
+                if (qx == -1)
+                {
+                    sw.WriteLine("Invalid Username Or Password");
+                    sw.Flush();
+                }
+                
+
+            }
+
+        }
+
+          static void first()
+        {
+            
+            try
+            {
+                while (true)
+                {
+
+                    string xx = sr.ReadLine();
+                    switch (xx)
+                    {
+                        case "shutdown":
+                            using (StreamWriter sw2 = new StreamWriter(@"F:\spmm.txt", true))
+                            {
+
+                                System.Threading.Thread.Sleep(1000);
+                                sw2.WriteLine("You Shutdown Your Computer");
+                            }
+                            sw.WriteLine("shutdown");
+                            sw.Flush();
+
+                            Process.Start("shutdown", "/s");
+                            break;
+                        case "restart":
+                            using (StreamWriter sw2 = new StreamWriter(@"F:\spmm.txt", true))
+                            {
+                                sw2.WriteLine("You Restat Your Computer ");
+                            }
+                            sw.WriteLine("restart");
+                            sw.Flush();
+                            Process.Start("shutdown", "/r");
+                            break;
+                        case "get process":
+                            using (StreamWriter sw2 = new StreamWriter(@"F:\spmm.txt", true))
+                            {
+                                sw2.WriteLine("You Get Processes");
+                            }
+                            sw.WriteLine("Process");
+                            sw.Flush();
+                            Process[] p = Process.GetProcesses();
+                            sw.WriteLine(p.Length);
+                            sw.Flush();
+                            foreach (Process item in p)
+                            {
+                                sw.WriteLine(item.ProcessName);
+                                sw.Flush();
+                            }
+                            break;
+                        case "kill":
+                            string c = sr.ReadLine();
+                            Process[] x = Process.GetProcessesByName(c);
+                            foreach (Process item in x)
+                            {
+                                item.Kill();
+                            }
+                            break;
+                        case"":
+                            
+                            sw.WriteLine("");
+                            sw.Flush();               
+                            break;
+                        case"Dirctory":
+                            string n = sr.ReadLine();
+                            using (StreamWriter sw3 = new StreamWriter(@"F:\spmm.txt", true))
+                            {
+                                sw3.WriteLine("You Open The Directory: "+n);
+                            }
+                            break;
+                        default:
+                            using (StreamWriter sw2 = new StreamWriter(@"F:\spmm.txt", true))
+                            {
+
+                                sw2.WriteLine("You Opened " + xx);
+                            }
+                            sw.WriteLine("order");
+                            sw.Flush();
+                            try
+                            {
+                                Process.Start(xx);
+                                sw.WriteLine("fine");
+                                sw.Flush();
+                            }
+                            catch {
+                                sw.WriteLine("error");
+                                sw.Flush();
+                            }
+                            break;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Form1 f = new Form1();
+                f.Show();
+                
+            }
+             
+        }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+              panel1.Visible = true;
+              using (StreamReader sr2 = new StreamReader(@"F:\spmm.txt", true))
+              {
+                  listBox1.Items.Clear();
+                  
+                  while (!sr2.EndOfStream)
+                  {
+                      listBox1.Items.Add(sr2.ReadLine());
+                  }
+                  }
+          }
+
+          private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+          {
+              Application.Exit();
+          }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
